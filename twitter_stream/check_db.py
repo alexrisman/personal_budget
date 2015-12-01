bafa057bdd7d9623f809c5bca46b10098b6d422b
Ubuntu <ubuntu@ip-172-31-39-205.us-west-2.compute.internal>
2015-12-01 05:02:17 +0000

    streamer and backfill modifications

--- twitter_stream/check_db.py
+++ twitter_stream/check_db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 client = MongoClient()
 
 db = client.dealtrader
-collection = db.tweets
+collection = db.raw_tweets
 
 print collection.count()
 print collection.find_one()
